6c59a0a6348f76854948ee038b2f7fd1dc9644d9
Kawayan <kawayan.deguzman@gmail.com>
2022-02-19 22:26:18 -0800

    Added created player input and computer result. Created function to determinte winner, loser, or draw.

--- script.js
+++ script.js
@@ -8,16 +8,61 @@ function computerPlay() {
     // associate number with rock, paper, or scissors
     // if the computer number is 1, return rock
     if (computerNumber === 1) {
-        return console.log('Rock'); 
+        return "rock"; 
     }
     // if the computer number is 2, return paper
     if (computerNumber === 2) {
-        return console.log('Paper'); 
+        return "paper"; 
     }
     // if the computer number is 3, return scissors
     if (computerNumber === 3) {
-        return console.log('Scissors'); 
+        return "scissors"; 
     }
 };
 
-function 
+// create a program that plays a single round of Rock Paper Scissors
+function playRound(playerSelection, computerSelection) {
+    // compare playerSelection and computerSelection
+    if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            return console.log("You Lose! " + computerSelection + " beats " + playerSelection);
+        } else if (computerSelection === "scissors") {
+            return console.log("You Win! " + playerSelection + " beats " + computerSelection);
+        } else {
+            console.log("Go again! " + "Computer also selected " + playerSelection);
+            const playerChoice = prompt("Choose your weapon: rock, paper, or scissors?");
+            console.log("You have chosen " + playerChoice + "..");
+            console.log(playRound(playerChoice, computerChoice));
+        }
+    }   else if (playerSelection === 'scissors') {
+        if (computerSelection === "paper") {
+            return console.log("You Win! " + playerSelection + " beats " + computerSelection);
+        } else if (computerSelection === "rock") {
+            return console.log("You Lose! " + computerSelection + " beats " + playerSelection);
+        } else if (playerSelection === 'scissors') {
+            console.log("Go again! " + "Computer also selected " + playerSelection );
+            const playerChoice = prompt("Choose your weapon: rock, paper, or scissors?");
+            console.log("You have chosen " + playerChoice + "..");
+            console.log(playRound(playerChoice, computerChoice));
+        }
+    } else if (playerSelection === 'paper') {
+        if (computerSelection === "rock") {
+            return console.log("You Win! " + playerSelection + " beats " + computerSelection);
+        } else if (computerSelection === "scissors") {
+            return console.log("You Lose! " + computerSelection + " beats " + playerSelection);
+        } else if (playerSelection === 'paper') {
+            console.log("Go again! " + "Computer also selected " + playerSelection );
+            const playerChoice = prompt("Choose your weapon: rock, paper, or scissors?")
+            console.log("You have chosen " + playerChoice + "..")
+            console.log(playRound(playerChoice, computerChoice));
+        }
+}
+};
+
+console.log("Aliens have landed on Earth and demand a game of rock paper scissors from a worthy opponent.")
+console.log("You are the chosen one..")
+const playerChoice = prompt("Choose your weapon: rock, paper, or scissors?")
+console.log("You have chosen " + playerChoice + "..")
+const computerChoice = computerPlay()
+
+console.log(playRound(playerChoice, computerChoice));
